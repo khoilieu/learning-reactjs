b4f5fe2438eb96a5ace455aaa40de76dba47913f
khoilvgcc210040 <khoilieuct03@gmail.com>
2025-03-17 07:47:04 +0700

    Completed platform details page

--- src/Pages/Plastforms.jsx
+++ src/Pages/Plastforms.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Breadcrumb = () => {
   return (
@@ -13,70 +14,72 @@ const Breadcrumb = () => {
           <span className="mx-2"><FontAwesomeIcon icon={faChevronRight} /><FontAwesomeIcon icon={faChevronRight} /></span>
         </li>
         <li>
-          <a href="/platforms" className="text-blue-400 hover:text-blue-700 text-md font-semibold">Sản Phẩm</a>
+          <a href="/platforms" className="text-green-600 hover:text-green-700 text-md font-semibold">Sản Phẩm</a>
         </li>
       </ol>
     </nav>
   );
 };
 
-const PlatformCard = ({ image, name, price }) => {
+const PlatformCard = ({ id, image, title, price }) => {
   return (
-    <div className="bg-white shadow-lg rounded-md overflow-hidden">
-      <img src={image} alt={name} className="w-full h-40 object-cover" />
-      <div className="p-4">
+    <div className="bg-white shadow-lg rounded-md overflow-hidden flex flex-col h-full">
+      <Link to={`/platform/${id}`} >
+        <img src={image} alt={title} className="w-full h-40 object-cover" />
+      </Link>
+      <div className="p-4 flex-1 flex flex-col justify-between">
         <h3 className="text-lg font-bold mb-2">
-          <a href="#" className="text-gray-600 hover:text-blue-700">{name}</a>
+          <Link to={`/platform/${id}`} className="text-gray-600 hover:text-green-700">{title}</Link>
         </h3>
-        <p className="text-blue-700 font-semibold">{price} VND</p>
+        <p className="text-green-700 font-semibold mt-auto">{price}</p>
       </div>
     </div>
   );
 };
 
 const Platforms = () => {
-  const products = [
-    {
-      name: "YouTube Premium",
-      price: "399.000",
-      image: "https://sadesign.vn/pictures/picfullsizes/2024/09/30/rgf1727672607.jpg"
-    },
-    {
-      name: "Tài khoản Udemy Business",
-      price: "299.000",
-      image: "https://sadesign.vn/pictures/picfullsizes/2024/12/13/udemy-noi-hoi-tu-nhung-khoa-hoc-chat-luong-cao-nhat-11.jpg"
-    },
-    {
-      name: "Nâng cấp Gemini Advanced PRO 2.0 chính chủ",
-      price: "199.000",
-      image: "https://i0.wp.com/software.centrix.asia/wp-content/uploads/2024/03/gemini-advanced-pro.webp"
-    },
-    {
-      name: "Mua dung lượng Google Drive (12 tháng)",
-      price: "150.000",
-      image: "https://v2.fullbootcamp.com/uploads/nang_cap_tai_khoan_google_e173165a5c.png"
-    },
-    {
-      name: "Nâng cấp tài khoản Canva giá rẻ (12 tháng)",
-      price: "99.000",
-      image: "https://divineshop.vn/image/catalog/Canva-giahan-1nam-13476.png?hash=1730437636"
-    }
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`/api/items/account`, {
+          headers: {
+            'Authorization': 'Bearer xBcjNfyHV5XxOhC3zf4Zfd4lwHwzgXJg'
+          }
+        });
+        const data = await response.json();
+        const formattedProducts = data.data.map(item => ({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          image: `${import.meta.env.VITE_API}/assets/${item.img}`
+        }));
+        setProducts(formattedProducts);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <div className="max-w-7xl mx-auto p-2 px-10 bg-white rounded-md mt-10">
-      <div className="max-w-5xl mx-auto p-2 px-10">
+      <div className="max-w-5xl">
         <Breadcrumb />
       </div>
       <h1 className="text-2xl font-bold text-center text-black mb-4 mt-5">Cửa hàng - Sản phẩm tài khoản bản quyền</h1>
-      <p className="text-center text-gray-600 mb-8">Giá rẻ, giá tốt nhất</p>
+      <p className="text-center text-green-600 mb-8">Giá rẻ, giá tốt nhất</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product, index) => (
           <PlatformCard key={index} {...product} />
         ))}
       </div>
       <div className="text-center mt-8">
-      <button className="mt-6 bg-blue-100 text-blue-700 text-sm py-3 px-10 rounded-md border-none hover:bg-blue-700 hover:text-white transition-colors-transform duration-300 hover:transform hover:scale-105">Tải thêm sản phẩm</button>
+        <button className="mt-6 bg-green-100 text-green-700 text-sm py-3 px-10 rounded-md border-none hover:bg-green-700 hover:text-white transition-colors-transform duration-300 hover:transform hover:scale-105">
+          Tải thêm sản phẩm
+        </button>
       </div>
     </div>
   );
